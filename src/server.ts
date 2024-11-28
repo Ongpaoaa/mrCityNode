28b0edd7fbe30451be1ac0f6743b818fba790a6a
Solnar <49712045+Ongpaoaa@users.noreply.github.com>
2024-11-27 19:04:07 -0800

    fix IDK bug

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import express, { Express, Request, Response } from "express";
 import helmet from "helmet";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import cors from "cors";
 
 import { indexRouter } from "@/routes";
 const app: Express = express();
-const port = process.env.PORT || 8080;
+dotenv.config();
+
+// Convert port to a number explicitly
+const port = parseInt(process.env.PORT || "8080", 10);
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-dotenv.config();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express TypeScript Server is Running");
@@ -19,7 +21,6 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use(indexRouter);
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`✨[server]: Server is running on port ${port}`);
 });
-
